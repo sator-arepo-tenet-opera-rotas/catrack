de6976ab40ad163ec816b3930f6ea4e56f09f6f5
SATOR AREPO TENET OPERA ROTAS <135734473+sator-arepo-tenet-opera-rotas@users.noreply.github.com>
2026-08-12 17:26:20 -0700

    Update dolbaniy8r.ts

--- starbuck/fuckermacher/dolbaniy8r.ts
+++ starbuck/fuckermacher/dolbaniy8r.ts
@@ -1,1 +1,13 @@
+new StaticArray<T>(length: i32) === ()> 
+getRandomValues(array: Uint8Array): void
 
+
+  
+--runtime             Specifies the runtime variant to include in the program.
+
+                         incremental  TLSF + incremental GC (default)
+                         minimal      TLSF + lightweight GC invoked externally
+                         stub         Minimal runtime stub (never frees)
+                         ...          Path to a custom runtime implementation
+
+  --exportRuntime       Exports the runtime helpers (__new, __collect etc.).
